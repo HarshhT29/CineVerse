ebb572027a052f780606d28369608f30f15ac3fa
Harsh <harshstudy29@gmail.com>
2024-04-07 17:49:42 +0530

    completed heroBanner section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react';
+import {HiOutlineSearch} from  "react-icons/hi";
+import {SlMenu} from "react-icons/sl";
+import {VscChromeClose} from "react-icons/vsc";
+import {useNavigate,useLocation} from "react-router-dom";
+import ContentWrapper from '../contentWrapper/ContentWrapper';
+import logo from "../../assets/movix-logo.svg";
 import "./style.css"
 
 const Header = () => {
+
+
   return (
-    <div>
-      HEADER
-    </div>
+    <header>
+      <ContentWrapper>
+        <div className="logo">
+          <img src={logo} alt='Sitelogo'/>
+        </div>
+        <ul className="menuItems">
+          <li className="items">Movies</li>
+          <li className="items">TV Shows</li>
+          <li className="items"><HiOutlineSearch/></li>
+        </ul>
+      </ContentWrapper>
+    </header>
   )
 }
 
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -4,6 +4,7 @@ import useHarsh from '../../../hooks/useHarsh';
 import { useSelector } from "react-redux";
 import LazyImg from "../../../components/lazyLoadImage/LazyImg";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+
 import "./style.css"
 
 const HeroBanner = () => {
@@ -25,23 +26,22 @@ const HeroBanner = () => {
     setInp(e.target.value);
   };
   return (
-    <div className="hero-banner w-full bg-[color:var(--black)] flex items-center relative h-[450px] md:h-[700px]">
-      {!loading && <div className="backdropImg w-full h-full absolute top-0 left-0 opacity-[0.5] overflow-hidden">
-        <LazyImg src={bg.toString()} alt="" className={'w-full h-full object-cover object-center'}/>
+    <div className="hero-banner w-full h-[450px] bg-[color:var(--black)] flex items-center relative md:h-[700px]">
+      {!loading && <div className="backdropImg w-full h-full absolute top-0 left-0 opacity-50 overflow-hidden">
+        <LazyImg src={bg.toString()} alt="" height={'100%'} width={'100%'} />
       </div>}
 
-      <div className="opacity-layer md:w-full md:h-64 absolute bottom-0 left-0"></div>
-
+      <div className="opacity-layer w-full h-[250px] absolute bottom-0 left-0"></div>
 
       <ContentWrapper>
         <div className="banner-content flex flex-col items-center text-white text-center relative max-w-[800px] my-0 mx-auto">
-          <span className="title text-[50px] font-bold md:mb-0 md:text-[90px]">Welcome</span>
-          <span className="subtitle text-[18px] font-bold md:text-[24px]">Millions of movies, TV shows and people to discover. Explore now!</span>
-          <div className="searchInput">
+          <span className="title text-[50px] font-bold mb-[10px] md:mb-0 md:text-[90px]">Welcome</span>
+          <span className="subtitle text-[18px] font-medium mb-10 md:text-[40px]">Millions of movies, TV shows and people to discover. Explore now!</span>
+          <div className="searchInput flex items-center w-full">
             <input type="text" placeholder="Search" 
               onChange={inpHandler} value={inp}
-              onKeyUp={inpHandler}/>
-            <button>Search</button>
+              onKeyUp={inpHandler} className="w-[calc(100%-100px)] h-[50px] bg-white outline-0 border-0 rounded-tl-3xl rounded-bl-3xl rounded-tr-none rounded-br-none py-0 px-4 text-[14px] md:w-[calc(100%-150px)] md:h-[60px] md:text-xl md:px-7 text-black"/>
+            <button className='w-[100px] h-[50px] bg-[image:var(--gradient)] outline-0 border-0 rounded-tr-3xl rounded-br-3xl rounded-tl-none rounded-bl-none text-[16px] cursor-pointer md:w-[150px] md:h-[60px] md:text-[18px]'>Search</button>
           </div>
         </div>
       </ContentWrapper>
